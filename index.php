eef0a5e1bb3065a6b523ac71fafae3441a5199b7
lcolome <lcolome@iesebre.com>
2023-10-23 17:40:27 +0200

    Sync

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Página de prueba</title>
+</head>
+<body>
+<h1>Formulario de prueba</h1>
 <?php
-echo "Hello world!";
+// Creamos un formulario
+echo "<form action='' method='post'>";
+echo "<input type='text' name='nombre' placeholder='Nombre' maxlength='20'>";
+echo "<input type='number' name='edad' placeholder='Edad'>";
+echo "<input type='submit' value='Enviar'>";
+echo "</form>";
+
+// Si el formulario se ha enviado, procesamos los datos
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Obtenemos los datos del formulario
+    $nombre = $_POST['nombre'];
+    $edad = $_POST['edad'];
+
+    // Imprimimos los datos en el navegador
+    echo "Tu nombre es: $nombre <br>";
+    echo "Tu edad es: " . $edad . "<br>";
+    echo "Tu edad es: " . $edad;
+}
+?>
+</body>
+</html>
